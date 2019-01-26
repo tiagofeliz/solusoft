c7848d058b9dfa2b2e709ba69a4708c6367ea2fc
tiagofeliz <hello@tiagofeliz.com>
2019-01-25 21:32:58 -0300

    Criando menu e importando font awesome

--- application/views/template/index.php
+++ application/views/template/index.php
@@ -5,15 +5,54 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Solusoft</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- main css -->
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/main.css'); ?>" type="text/css">
         <!-- bootstrap css -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+        <!-- font awesome -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-12">
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                        <a class="navbar-brand" href="#">Solusoft</a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav mr-auto">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="<?php echo site_url('clientes'); ?>"><i class="fas fa-users"></i> Clientes</a>
+                                </li>
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="<?php echo site_url('produtos'); ?>"><i class="fas fa-cubes"></i> Produtos</a>
+                                </li>
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="<?php echo site_url('pedidos'); ?>"><i class="fas fa-shopping-cart"></i> Pedidos</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i class="fas fa-chart-bar"></i> Relatórios
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="#">Pedidos</a>
+                                        <a class="dropdown-item" href="#">Totalizador por cliente</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+
         <!-- jquery -->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <!-- bootstrap js -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-    </head>
-    <body>
-        Hello Solusoft
     </body>
 </html>
