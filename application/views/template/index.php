c88774282aa971d43d1bc32b2a14640d1073477d
tiagofeliz <hello@tiagofeliz.com>
2019-01-26 11:12:46 -0300

    Incluindo o JQuery Masked Input para formatações

--- application/views/template/index.php
+++ application/views/template/index.php
@@ -59,5 +59,7 @@
         <!-- bootstrap js -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+        <!-- masked input -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.4.1/jquery.maskedinput.min.js" crossorigin="anonymous"></script>        
     </body>
 </html>
