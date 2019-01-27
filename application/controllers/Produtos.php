448824bb63f34b3d53115a9bab798d0be275780b
tiagofeliz <hello@tiagofeliz.com>
2019-01-27 18:58:38 -0300

    Carregando helper

--- application/controllers/Produtos.php
+++ application/controllers/Produtos.php
@@ -9,7 +9,7 @@ class Produtos extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->library(array('Util'));
+		$this->load->helper('util');
 	}
 
 	public function index()
