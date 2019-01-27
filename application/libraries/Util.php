479390ff774faa992a68e4605e54dd5f7f012d17
tiagofeliz <hello@tiagofeliz.com>
2019-01-27 18:56:30 -0300

    Parâmetros de email

--- application/libraries/Util.php
+++ application/libraries/Util.php
@@ -4,9 +4,17 @@ if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 class Util 
 {
-
-    function formataValor($valor = 0)
-    {
-        return 'R$ ' . number_format(round($valor, 2), 2, ',', '.');
+    function parametrosEmail()
+    {        
+        return array(
+            'protocol' => 'smtp',
+            'smtp_host' => 'ssl://smtp.gmail.com',
+            'smtp_port' => 465,
+            'smtp_user' => 'tiagofeliz.solusoft@gmail.com',
+            'smtp_pass' => '@solusoft$@',
+            'mailtype'  => 'html', 
+            'charset'   => 'utf-8',
+            'newline'   => "\r\n"
+        );
     }
 }
